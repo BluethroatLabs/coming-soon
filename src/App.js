6ba932f8c41b857077bf50d465f388aced758306
Rahul Saxena <rahulsaxena.pro@gmail.com>
2022-12-13 16:16:52 -0600

    Initial modifications to the website done. Ready to deploy

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import React from 'react';
 
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
-
+import logo from '../src/assets/1-removebg-preview.png';
 import './styles.css';
 
 // import video
-import videoBg from '../src/assets/video.mp4';
+import videoBg from '../src/assets/Itachi.mp4';
+
+const googleFormURL = `https://docs.google.com/forms/d/e/1FAIpQLSdjb_Kuu4yQNQMvpDBVe8hzhrkVnIMxn-lGx5Ho1yA9V-bR9g/viewform?usp=sf_link`;
 
 const App = () => {
   return (
-    <section className='page'>
-      {/* overlay */}
-      <div className='overlay'></div>
-      <video src={videoBg} autoPlay loop muted />
-      <div className='page__content'>
-        <h1>Launching Soon</h1>
-        <h3>
-          Leave your email and we'll let you know once the site goes live.
-        </h3>
-        <FlipClockCountdown
-          className='flip-clock'
-          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
-        />
-        <button className='btn'>Notify me</button>
-      </div>
-    </section>
+    <div className='main'>
+        <div className='overlay'></div>
+        <video src={videoBg} autoPlay loop muted/>
+        <div className='logo'>
+            <img src={logo} />
+            <h1>Launching Soon</h1>
+            <h3>
+              Pre-launch offer: Get a free high-level preliminary security review for your protocol when you register 
+            </h3>
+            <FlipClockCountdown
+              className='flip-clock'
+              to=
+                {new Date().getTime() + Math.round((new Date("2023-01-01").getTime() - new Date().getTime()))}
+            />
+            <a href={googleFormURL} target='_blank'><button className='btn' >register interest</button></a>
+        </div>
+    </div>
   );
 };
 
