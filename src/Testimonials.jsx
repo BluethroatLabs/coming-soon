5550d50402bb2c68da417a9edea5341e44a4744d
Rahul Saxena <rahulsaxena.pro@gmail.com>
2022-12-19 16:35:10 -0600

    Added automatic testimonial display

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { useEffect } from "react";
 import './Testimonial.css';
 
 const Testimonial = () => {
@@ -9,7 +10,7 @@ const Testimonial = () => {
         "I went and sought out three general code reviews. Rahul's was the highest quality I received! 25+ action items and not a single one was fluff or padding.",
     },
     1: {
-        client: `Maddiaa0, Engineering @AztecNetwork`,
+        client: `Maddiaa, Engineering @AztecNetwork`,
         quote: 
         `Also excellent write up yet again. Dare I say some of the best educational content atm.`
     },
@@ -20,26 +21,25 @@ const Testimonial = () => {
     }
   }
 
-  const [current, setCurrent] = useState(quotes[0])
+  const [state, setState] = React.useState({ num: 0 });
+  const counter = React.useRef(0);
+  
+  React.useEffect(() => {
+    if (counter.current < 10) {
+      counter.current += 1;
+      const timer = setTimeout(() => setState({ num: state.num + 1 }), 7000);
 
-  const [active, setActive] = useState(0)
-
-  const handleSetClick = event => {
-    setCurrent(quotes[event.target.getAttribute("data-quote")])
-    setActive(event.target.getAttribute("data-quote"))
-  }
+      return () => clearTimeout(timer);
+    }
+  }, [state]);
 
   return (
     <div className="quote" >
-      <p><i>{current.quote}</i></p>
-      <p>{current.client}</p>
-      <div className="cards">
-        {Object.keys(quotes).map(index => (
-          <button onClick={event => handleSetClick(event)} data-quote={index} key={index}> {++index } </button>
-        ))}
-      </div>
+      <p><i>{quotes[state.num % 3].quote}</i></p>
+      <p>{quotes[state.num % 3].client}</p>
     </div>
   )
 }
 
 export default Testimonial;
+
