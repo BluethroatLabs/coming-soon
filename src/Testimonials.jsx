6ceffbf2ea59c37a854e40978703dc0a93b8b5d8
Rahul Saxena <rahulsaxena.pro@gmail.com>
2022-12-19 12:13:07 -0600

    capitalised important words

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -16,7 +16,7 @@ const Testimonial = () => {
     2: {
         client: `Kalpita, Engineering @Marlin`,
         quote: 
-        "You will come across only a few as diligent and RiDiCuLoUsLy eFfIcIeNt engineers as Rahul. I had the pleasure of working alongside Rahul as a blockchain engineer at Sublime. During the past year, I have watched him constantly strive to optimize and revamp our codebase, be it smart contracts or testing frameworks. His attention to detail has helped eliminate multiple vulnerabilities from our protocol. He has always been the go-to person for exploring new ideas and his Make-It-Happen attitude is a bonus. He is usually the first person to help out or collaborate with other team members. He will be a great value addition to any team he joins."
+        "You will come across only a few as diligent and RIDICULOUSLY EFFICIENT engineers as Rahul. I had the pleasure of working alongside Rahul as a blockchain engineer at Sublime. During the past year, I have watched him constantly strive to optimize and revamp our codebase, be it smart contracts or testing frameworks. His attention to detail has helped ELIMINATE MULTIPLE VULNERABILITIES from our protocol. He has always been the go-to person for exploring new ideas and his Make-It-Happen attitude is a bonus. He is usually the first person to help out or collaborate with other team members. He will be a great value addition to any team he joins."
     }
   }
 
