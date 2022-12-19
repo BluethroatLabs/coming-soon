1ba47a31819bf474cd17a54d67544a86a651a874
Rahul Saxena <rahulsaxena.pro@gmail.com>
2022-12-19 12:57:49 -0600

    Reordered the testimonials

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -3,7 +3,7 @@ import './Testimonial.css';
 
 const Testimonial = () => {
   const quotes = {
-    0: {
+    2: {
       client: "Zodomo.eth 🦇🔊, Canto Name Service",
       quote:
         "I went and sought out three general code reviews. Rahul's was the highest quality I received! 25+ action items and not a single one was fluff or padding.",
@@ -13,7 +13,7 @@ const Testimonial = () => {
         quote: 
         `Also excellent write up yet again. Dare I say some of the best educational content atm.`
     },
-    2: {
+    0: {
         client: `Kalpita, Engineering @Marlin`,
         quote: 
         "You will come across only a few as diligent and RIDICULOUSLY EFFICIENT engineers as Rahul. I had the pleasure of working alongside Rahul as a blockchain engineer at Sublime. During the past year, I have watched him constantly strive to optimize and revamp our codebase, be it smart contracts or testing frameworks. His attention to detail has helped ELIMINATE MULTIPLE VULNERABILITIES from our protocol. He has always been the go-to person for exploring new ideas and his Make-It-Happen attitude is a bonus. He is usually the first person to help out or collaborate with other team members. He will be a great value addition to any team he joins."
