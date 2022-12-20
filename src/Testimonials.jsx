79a3926b826da378f541a5cd5559a0f4d1776280
Rahul Saxena <rahulsaxena.pro@gmail.com>
2022-12-20 11:11:56 -0600

    Added metadev's testimonial

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -9,12 +9,17 @@ const Testimonial = () => {
       quote:
         "I went and sought out three general code reviews. Rahul's was the highest quality I received! 25+ action items and not a single one was fluff or padding.",
     },
-    1: {
+    2: {
+      client: `metadev.eth ᵍᵐ (🫡,🇮🇳) , Twitter: @poyimone`,
+      quote: 
+      `Detailed report. It's really great to have a second set of eyes look through the code and to make sure all the bugs are patched. No one could have done it better than Rahul. Excellent writeup and a detailed review. It was precise and I learned a lot.`
+    },
+    2: {
         client: `Maddiaa, Engineering @AztecNetwork`,
         quote: 
         `Also excellent write up yet again. Dare I say some of the best educational content atm.`
     },
-    2: {
+    3: {
         client: `Kalpita, Engineering @Marlin`,
         quote: 
         "You will come across only a few RIDICULOUSLY EFFICIENT engineers as Rahul. His attention to detail has helped ELIMINATE MULTIPLE VULNERABILITIES from our protocol."
