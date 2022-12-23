8bd70e698b0bab6ee982933159218185e96e2d6c
Rahul Saxena <rahulsaxena.pro@gmail.com>
2022-12-23 12:32:11 -0600

    Added a sample report, blogs

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import videoBg from '../src/assets/video.mp4';
 import Testimonial from './Testimonials';
 
 const googleFormURL = `https://docs.google.com/forms/d/e/1FAIpQLSdjb_Kuu4yQNQMvpDBVe8hzhrkVnIMxn-lGx5Ho1yA9V-bR9g/viewform?usp=sf_link`;
+const blogsURL = `https://saxenism.com/`;
+const sampleReportURL = `https://github.com/BluethroatLabs/audits/tree/main/canto_name_service`;
 
 const App = () => {
   return (
     <>
     <div className='main'>
-        <div className='overlay'></div>
-        <video src={videoBg} autoPlay loop muted/>
+    <div className='overlay'></div>
+      <video src={videoBg} autoPlay loop muted/>
         <div className='logo'>
             <img src={logo} />
             <h1>Launching Soon</h1>
@@ -28,7 +30,13 @@ const App = () => {
               to=
                 {new Date().getTime() + Math.round((new Date("2023-01-01").getTime() - new Date().getTime()))}
             />
-            <a href={googleFormURL} target='_blank'><button className='btn' >register interest</button></a>
+            <a href={googleFormURL} target='_blank'><button className='btn' >register interest</button></a> 
+            &nbsp;
+            <div id="butn">
+              <a href={blogsURL} target='_blank'><button type="button home-button" id="button1" >read our blogs</button></a>
+              &nbsp;
+              <a href={sampleReportURL} target='_blank'><button type="button contact-button" id="button2">sample report</button></a>
+            </div>
             <br />
             <br />
             <h1>Word on the street</h1>
